b54cd30aeb5d09d2615f2845e458200ba3e8ece7
Enrico Regge <REGGEENR@de.ibm.com>
2021-03-30 10:42:06 +0200

    added some sources

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  console.log('Hello world received a request.');
+
+  const target = process.env.TARGET || 'World';
+  res.send(`Hello ${target}! on port ${port}`);
+});
+
+
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
